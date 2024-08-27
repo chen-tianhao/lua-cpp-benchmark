bc09066f9a321c260707553562845598cbe4a931
chen-tianhao <fm0517@hotmail.com>
2024-08-27 22:16:06 +0800

    Okay for inserting performance test for SQLite driver and ODBC

--- CppAccessODBC/CppAccessODBC.cpp
+++ CppAccessODBC/CppAccessODBC.cpp
@@ -1,8 +1,9 @@
-#include <sstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <windows.h>
 #include <sqlext.h>
+#include <chrono>
 
 void checkError(SQLRETURN retCode, SQLHANDLE handle, SQLSMALLINT type) {
     if (retCode != SQL_SUCCESS && retCode != SQL_SUCCESS_WITH_INFO) {
@@ -21,14 +22,104 @@ void checkError(SQLRETURN retCode, SQLHANDLE handle, SQLSMALLINT type) {
     }
 }
 
-int main() {
+int CreatTable(SQLHSTMT hStmt)
+{
+    // Create a table using std::string and convert to wide string
+    std::string recreateTableSQL = "DROP TABLE IF EXISTS COMPANY;"
+        "CREATE TABLE IF NOT EXISTS COMPANY("
+        "ID INT PRIMARY KEY     NOT NULL,"
+        "NAME           TEXT    NOT NULL,"
+        "AGE            INT     NOT NULL,"
+        "ADDRESS        CHAR(50),"
+        "SALARY         REAL );";
+    std::wstring wCreateTableSQL(recreateTableSQL.begin(), recreateTableSQL.end());
+
+    SQLRETURN retCode = SQLExecDirect(hStmt, (SQLWCHAR*)wCreateTableSQL.c_str(), SQL_NTS);
+    std::cout << "Recreate table." << std::endl;
+    checkError(retCode, hStmt, SQL_HANDLE_STMT);
+    return retCode;
+}
+
+int InsertRows(SQLHSTMT hStmt, int rowNum)
+{
+    int retCode = 0;
+    // Execute each INSERT statement individually
+    for (int i = 1; i <= 1000; ++i) {
+        std::stringstream ss;
+        ss << "INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) VALUES(" << i << ",'" << i << "','" << i << "','" << i << "','" << i << "');" << std::endl;
+        std::string insertSQL = ss.str();
+        std::wstring wInsertSQL(insertSQL.begin(), insertSQL.end());
+
+        retCode = SQLExecDirect(hStmt, (SQLWCHAR*)wInsertSQL.c_str(), SQL_NTS);
+        checkError(retCode, hStmt, SQL_HANDLE_STMT);
+    }
+    return retCode;
+}
+
+int InsertRowsTransaction(SQLHSTMT hStmt, SQLHDBC hDbc, int rowNum)
+{
+    int retCode = 0;
+    // Turn off auto-commit mode for transaction handling
+    SQLSetConnectAttr(hDbc, SQL_ATTR_AUTOCOMMIT, (SQLPOINTER)SQL_AUTOCOMMIT_OFF, 0);
+    // Execute each INSERT statement individually
+    for (int i = 1; i <= 1000; ++i) {
+        std::stringstream ss;
+        ss << "INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) VALUES(" << i << ",'" << i << "','" << i << "','" << i << "','" << i << "');" << std::endl;
+        std::string insertSQL = ss.str();
+        std::wstring wInsertSQL(insertSQL.begin(), insertSQL.end());
+
+        retCode = SQLExecDirect(hStmt, (SQLWCHAR*)wInsertSQL.c_str(), SQL_NTS);
+        checkError(retCode, hStmt, SQL_HANDLE_STMT);
+    }
+
+    // Commit the transaction
+    retCode = SQLEndTran(SQL_HANDLE_DBC, hDbc, SQL_COMMIT);
+    checkError(retCode, hDbc, SQL_HANDLE_DBC);
+
+    // Turn auto-commit back on
+    SQLSetConnectAttr(hDbc, SQL_ATTR_AUTOCOMMIT, (SQLPOINTER)SQL_AUTOCOMMIT_ON, 0);
+    return retCode;
+}
+
+int ReadTable(SQLHSTMT hStmt)
+{
+    // Query data using std::string and convert to wide string
+    std::string selectSQL = "SELECT * FROM COMPANY;";
+    std::wstring wSelectSQL(selectSQL.begin(), selectSQL.end());
+
+    int retCode = SQLExecDirect(hStmt, (SQLWCHAR*)wSelectSQL.c_str(), SQL_NTS);
+    checkError(retCode, hStmt, SQL_HANDLE_STMT);
+
+    // Bind columns and fetch data
+    SQLINTEGER id;
+    SQLWCHAR name[64];
+    SQLINTEGER age;
+    SQLWCHAR address[256];
+    SQLINTEGER salary;
+
+    SQLBindCol(hStmt, 1, SQL_C_LONG, &id, 0, NULL);
+    SQLBindCol(hStmt, 2, SQL_C_WCHAR, name, sizeof(name) / sizeof(SQLWCHAR), NULL);
+    SQLBindCol(hStmt, 3, SQL_C_LONG, &age, 0, NULL);
+    SQLBindCol(hStmt, 4, SQL_C_WCHAR, address, sizeof(address) / sizeof(SQLWCHAR), NULL);
+    SQLBindCol(hStmt, 5, SQL_C_LONG, &salary, 0, NULL);
+
+    /*
+    while (SQLFetch(hStmt) == SQL_SUCCESS) {
+        std::wcout << L"ID: " << id << L", Name: " << name << L", Age: " << age
+            << L", Address: " << address << L", Salary: " << salary << std::endl;
+    }
+    */
+    return retCode;
+}
+
+int main() 
+{
     SQLHENV hEnv = NULL;
     SQLHDBC hDbc = NULL;
     SQLHSTMT hStmt = NULL;
-    SQLRETURN retCode;
 
     // Allocate an environment handle
-    retCode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnv);
+    int retCode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnv);
     checkError(retCode, hEnv, SQL_HANDLE_ENV);
 
     // Set the ODBC version environment attribute
@@ -51,57 +142,34 @@ int main() {
     retCode = SQLAllocHandle(SQL_HANDLE_STMT, hDbc, &hStmt);
     checkError(retCode, hStmt, SQL_HANDLE_STMT);
 
-    // Create a table using std::string and convert to wide string
-    std::string recreateTableSQL = "DROP TABLE IF EXISTS COMPANY;"
-        "CREATE TABLE IF NOT EXISTS COMPANY("
-        "ID INT PRIMARY KEY     NOT NULL,"
-        "NAME           TEXT    NOT NULL,"
-        "AGE            INT     NOT NULL,"
-        "ADDRESS        CHAR(50),"
-        "SALARY         REAL );";
-    std::wstring wCreateTableSQL(recreateTableSQL.begin(), recreateTableSQL.end());
+    int rowNumber = 1000;
+    int runNumber = 10;
 
-    retCode = SQLExecDirect(hStmt, (SQLWCHAR*)wCreateTableSQL.c_str(), SQL_NTS);
-    std::cout << "Recreate table." << std::endl;
-    checkError(retCode, hStmt, SQL_HANDLE_STMT);
-
-    // Insert data using std::string and convert to wide string
-    std::stringstream ss;
-    for (int i = 0; i < 1; i++)
+    double totalTimeCose = 0;
+    for (int i = 0; i < runNumber; i++)
     {
-        ss << "INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) VALUES(" << i << ",'" << i << "','" << i << "','" << i << "','" << i << "');" << std::endl;
+        CreatTable(hStmt);
+        auto insertStart = std::chrono::high_resolution_clock::now();
+        InsertRows(hStmt, rowNumber);
+        auto insertEnd = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> insert_elapsed = insertEnd - insertStart;
+        std::cout << "Insert time: " << insert_elapsed.count() << " seconds" << std::endl;
+        totalTimeCose += insert_elapsed.count();
     }
-    std::string insertSQL = ss.str();
-    std::wstring wInsertSQL(insertSQL.begin(), insertSQL.end());
-
-    retCode = SQLExecDirect(hStmt, (SQLWCHAR*)wInsertSQL.c_str(), SQL_NTS);
-    std::cout << "Insert records." << std::endl;
-    checkError(retCode, hStmt, SQL_HANDLE_STMT);
-
-    // Query data using std::string and convert to wide string
-    std::string selectSQL = "SELECT * FROM COMPANY;";
-    std::wstring wSelectSQL(selectSQL.begin(), selectSQL.end());
-
-    retCode = SQLExecDirect(hStmt, (SQLWCHAR*)wSelectSQL.c_str(), SQL_NTS);
-    checkError(retCode, hStmt, SQL_HANDLE_STMT);
-
-    // Bind columns and fetch data
-    SQLINTEGER id;
-    SQLWCHAR name[64];
-    SQLINTEGER age;
-    SQLWCHAR address[256];
-    SQLFLOAT salary;
-
-    SQLBindCol(hStmt, 1, SQL_C_LONG, &id, 0, NULL);
-    SQLBindCol(hStmt, 2, SQL_C_WCHAR, name, sizeof(name) / sizeof(SQLWCHAR), NULL);
-    SQLBindCol(hStmt, 3, SQL_C_LONG, &age, 0, NULL);
-    SQLBindCol(hStmt, 4, SQL_C_WCHAR, address, sizeof(address) / sizeof(SQLWCHAR), NULL);
-    SQLBindCol(hStmt, 5, SQL_C_FLOAT, &salary, 0, NULL);
-
-    while (SQLFetch(hStmt) == SQL_SUCCESS) {
-        std::wcout << L"ID: " << id << L", Name: " << name << L", Age: " << age
-            << L", Address: " << address << L", Salary: " << salary << std::endl;
+    std::cout << "Averave insert time (non-transaction): " << totalTimeCose / runNumber << " seconds" << std::endl;
+    
+    totalTimeCose = 0;
+    for (int i = 0; i < runNumber; i++)
+    {
+        CreatTable(hStmt);
+        auto insertStart = std::chrono::high_resolution_clock::now();
+        InsertRowsTransaction(hStmt, hDbc, rowNumber);
+        auto insertEnd = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> insert_elapsed = insertEnd - insertStart;
+        std::cout << "Insert time: " << insert_elapsed.count() << " seconds" << std::endl;
+        totalTimeCose += insert_elapsed.count();
     }
+    std::cout << "Averave insert time (transaction): " << totalTimeCose / runNumber << " seconds" << std::endl;
 
     // Cleanup
     SQLFreeHandle(SQL_HANDLE_STMT, hStmt);
--- CppAccessSQLite/CppAccessSQLite.cpp
+++ CppAccessSQLite/CppAccessSQLite.cpp
@@ -1,7 +1,7 @@
 #include <sstream>
 #include <iostream>
-#include <sqlite3.h>
 #include <chrono>
+#include "../sqlite3/sqlite3.h"
 
 // 回调函数，用于从查询中获取数据
 static int callback(void* NotUsed, int argc, char** argv, char** azColName) {
@@ -16,13 +16,123 @@ static int callbackWithoutShow(void* NotUsed, int argc, char** argv, char** azCo
     return 0;
 }
 
+int CreatTable(sqlite3* db)
+{
+    char* errMsg = 0;
+    // 创建一个表
+    const char* sqlCreateTable = "DROP TABLE IF EXISTS COMPANY;"
+        "CREATE TABLE COMPANY("
+        "ID INT PRIMARY KEY     NOT NULL,"
+        "NAME           TEXT    NOT NULL,"
+        "AGE            INT     NOT NULL,"
+        "ADDRESS        CHAR(50),"
+        "SALARY         REAL );";
+
+    int rc = sqlite3_exec(db, sqlCreateTable, nullptr, 0, &errMsg);
+    if (rc != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+    }
+    else {
+        std::cout << "Table created successfully" << std::endl;
+    }
+    return rc;
+}
+
+int InsertRows(sqlite3* db, int rowNum)
+{
+    char* errMsg = 0;
+    // 插入数据
+    std::stringstream ss;
+    for (int i = 0; i < rowNum; i++)
+    {
+        ss << "INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) VALUES(" << i << ",'" << i << "','" << i << "','" << i << "','" << i << "');" << std::endl;
+    }
+    std::string sql_str = ss.str();
+
+    int rc = sqlite3_exec(db, sql_str.c_str(), nullptr, 0, &errMsg);
+    if (rc != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+    }
+    else {
+        std::cout << "Records created successfully" << std::endl;
+    }
+    return rc;
+}
+
+int InsertRowsTransaction(sqlite3* db, int rowNum)
+{
+    char* errMsg = 0;
+
+    // 开始事务
+    int rc = sqlite3_exec(db, "BEGIN TRANSACTION;", 0, 0, &errMsg);
+    if (rc != SQLITE_OK)
+    {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        return rc;
+    }
+
+    std::stringstream ss;
+    // 插入数据
+    for (int i = 0; i < rowNum; i++)
+    {
+        ss << "INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) VALUES(" << i << ",'" << i << "','" << i << "','" << i << "','" << i << "');" << std::endl;
+    }
+    std::string sql_str = ss.str();
+
+    rc = sqlite3_exec(db, sql_str.c_str(), nullptr, 0, &errMsg);
+    if (rc != SQLITE_OK) 
+    {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+        // 如果插入过程中发生错误，回滚事务
+        sqlite3_exec(db, "ROLLBACK;", 0, 0, &errMsg);
+        sqlite3_close(db);
+        return rc;
+    }
+    else {
+        std::cout << "Records created successfully" << std::endl;
+    }
+
+    // 提交事务
+    rc = sqlite3_exec(db, "COMMIT;", 0, 0, &errMsg);
+    if (rc != SQLITE_OK) 
+    {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+        sqlite3_close(db);
+        return rc;
+    }
+    return rc;
+}
+
+int ReadTable(sqlite3* db)
+{
+    char* errMsg = 0;
+    // 读取数据
+    const char* sqlSelectData = "SELECT * FROM COMPANY";
+    auto select_start = std::chrono::high_resolution_clock::now();
+    int rc = sqlite3_exec(db, sqlSelectData, callbackWithoutShow, 0, &errMsg);
+    if (rc != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << std::endl;
+        sqlite3_free(errMsg);
+    }
+    auto select_end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> select_elapsed = select_end - select_start;
+    std::cout << "Select time: " << select_elapsed.count() << " seconds" << std::endl;
+    return rc;
+}
+
 int main() {
     sqlite3* db;
-    char* errMsg = 0;
-    int rc;
+    int rowNumber = 1000;
+    int runNumber = 10;
 
     // 打开数据库连接，如果数据库不存在则创建一个新的数据库
-    rc = sqlite3_open("example.db", &db);
+    int rc = sqlite3_open("example.db", &db);
     if (rc) {
         std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
         return 1;
@@ -31,59 +141,33 @@ int main() {
         std::cout << "Opened database successfully" << std::endl;
     }
 
-    // 创建一个表
-    const char* sqlCreateTable = "CREATE TABLE IF NOT EXISTS COMPANY("
-        "ID INT PRIMARY KEY     NOT NULL,"
-        "NAME           TEXT    NOT NULL,"
-        "AGE            INT     NOT NULL,"
-        "ADDRESS        CHAR(50),"
-        "SALARY         REAL );"
-        "delete from COMPANY;";
-
-    rc = sqlite3_exec(db, sqlCreateTable, nullptr, 0, &errMsg);
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << errMsg << std::endl;
-        sqlite3_free(errMsg);
-    }
-    else {
-        std::cout << "Table created successfully" << std::endl;
-    }
-
-    // 插入数据
-    std::stringstream ss;
-    for (int i = 0; i < 1000; i++)
+    double totalTimeCose = 0;
+    for (int i = 0; i < runNumber; i++)
     {
-        ss << "INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY) VALUES(" << i << ",'" << i << "','" << i << "','" << i << "','" << i << "');" << std::endl;
+        CreatTable(db);
+        auto insertStart = std::chrono::high_resolution_clock::now();
+        InsertRows(db, rowNumber);
+        auto insertEnd = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> insert_elapsed = insertEnd - insertStart;
+        std::cout << "Insert time: " << insert_elapsed.count() << " seconds" << std::endl;
+        totalTimeCose += insert_elapsed.count();
     }
-    std::string sql_str = ss.str();
+    std::cout << "Averave insert time (non-transaction): " << totalTimeCose/runNumber << " seconds" << std::endl;
 
-    auto insert_start = std::chrono::high_resolution_clock::now();
-    rc = sqlite3_exec(db, sql_str.c_str(), nullptr, 0, &errMsg);
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << errMsg << std::endl;
-        sqlite3_free(errMsg);
+    totalTimeCose = 0;
+    for (int i = 0; i < runNumber; i++)
+    {
+        CreatTable(db);
+        auto insertStart = std::chrono::high_resolution_clock::now();
+        InsertRowsTransaction(db, rowNumber);
+        auto insertEnd = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> insert_elapsed = insertEnd - insertStart;
+        std::cout << "Insert time: " << insert_elapsed.count() << " seconds" << std::endl;
+        totalTimeCose += insert_elapsed.count();
     }
-    else {
-        std::cout << "Records created successfully" << std::endl;
-    }
-    auto insert_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> insert_elapsed = insert_end - insert_start;
-    std::cout << "Insert time: " << insert_elapsed.count() << " seconds" << std::endl;
-
-    // 读取数据
-    const char* sqlSelectData = "SELECT * FROM COMPANY";
-    auto select_start = std::chrono::high_resolution_clock::now();
-    rc = sqlite3_exec(db, sqlSelectData, callbackWithoutShow, 0, &errMsg);
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << errMsg << std::endl;
-        sqlite3_free(errMsg);
-    }
-    auto select_end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> select_elapsed = select_end - select_start;
-    std::cout << "Select time: " << select_elapsed.count() << " seconds" << std::endl;
+    std::cout << "Averave insert time (transaction): " << totalTimeCose/runNumber << " seconds" << std::endl;
 
     // 关闭数据库连接
     sqlite3_close(db);
-
     return 0;
 }
